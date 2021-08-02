d5ddaf524efdd68858bb8e4d87772440f54e7207
Cian <cian.omondi@student.moringaschool.com>
2021-08-02 22:13:17 +0300

    commit 4

--- app/views.py
+++ app/views.py
@@ -14,7 +14,8 @@ def index():
     upcoming_movie = get_movies('upcoming')
     now_showing_movie = get_movies('now_playing')
     title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template('index.html',title = title,popular = popular_movies, upcoming = upcoming_movie,now_showing= now_showing_movie)
+    message = 'Welcome to Tazama'
+    return render_template('index.html',message = message,title = title,popular = popular_movies, upcoming = upcoming_movie,now_showing= now_showing_movie)
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
