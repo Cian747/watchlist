69cf6820d3bcd8b9e60311cee40556d2daf25945
Cian <cian.omondi@student.moringaschool.com>
2021-08-12 07:51:56 +0300

    user can now updateprofile and upload a picture

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 from config import config_options
 from flask_login import LoginManager
+from flask_mail import Mail
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+
+photos = UploadSet('photos', IMAGES)
+mail = Mail()
 
 def create_app(config_name):
 
@@ -21,6 +26,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
     
     # Registering the blueprint
     from .main import main as main_blueprint
@@ -29,6 +35,9 @@ def create_app(config_name):
     # Registering auth blueprint
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+    # configure UploadSet
+    configure_uploads(app,photos)
 
     # setting config
     from .request import configure_request
--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 export MOVIE_API_KEY='8754b90a17ca455936d7cd7376d541ed'
 export SECRET_KEY='CnSWhpHY'
+export MAIL_USERNAME='cian254@gmail.com'
+export MAIL_PASSWORD='12345'
 
 python3.9 manage.py server
