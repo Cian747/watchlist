b005898c8d9d4ef072bc4a73a7758df6d8b99828
Cian <cian.omondi@student.moringaschool.com>
2021-08-23 13:42:05 +0300

    a new schema

--- manage.py
+++ manage.py
@@ -10,10 +10,6 @@ manager = Manager(app)
 
 manager.add_command('server',Server)
 
-@manager.before_first_request
-def create_tables():
-    db.create_all()
-
 @manager.command
 def test():
     """
